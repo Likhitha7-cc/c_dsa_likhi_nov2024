c6131057622fb5ebdbb6d3d2732b9c74f3a79fb4
Likhitha7-cc <likhithac89@gmail.com>
2024-11-06 16:10:29 +0530

    Add files via upload

---
+++ static.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void add(){
+    int c=1;
+    printf("%d ",c);
+}
+void sub(){
+    int b=2;
+    printf("%d ",b);
+}
+int main(){
+    add();
+    sub();
+}
---
+++ variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void add(){
+    int a=3;
+    printf("%d",a);
+}
+void sub(){
+    int b=8;
+    printf("%d",b);
+}
+int main(){
+    add();
+    sub();
+}
