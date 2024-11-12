fdd514f154cac353c9e9072a9881d476a48f2c72
c_dsa_likhi_nov2024 <likhithac89@gmail.com>
2024-11-12 16:27:38 +0530

    nov12

--- nov12/squre2d.c
+++ nov12/squre2d.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
+int isLower(int n,int arr[][n]){
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i][j]!=0){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 int main(){
     int n;
     printf("enter n:");
     scanf("%d",&n);
     int arr[n][n];
     printf("enter elements:");
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            scanf("%d"&arr[i]);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[i][j]);
         }
     }
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-        }
-    }
+    printf("%d  ",isLower(n,arr));
 }
